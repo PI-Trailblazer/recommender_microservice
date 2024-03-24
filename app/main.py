cfac8f7e5fe53736005a437561c3ff4a13b4f12d
AlexandreCotorobai <alexandrecotorobai@gmail.com>
2024-03-24 15:26:08 +0000

    TRAIL-28 forgot main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.api import router as api_router
+
+app = FastAPI(
+    title="Recommender Microservice",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="0.1.0",
+)
+
+app.include_router(api_router, prefix="/api")
