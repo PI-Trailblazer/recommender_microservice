f21caeb5d225f185e2dd8557a1cd6af7a29f2df5
DuarteCruz31 <duarteccruz8@gmail.com>
2024-03-27 15:11:33 +0000

    TRAIL-41 setup

--- app/api/recommender.py
+++ app/api/recommender.py
@@ -1,9 +1,3 @@
-from requests import Session
-from app.api import deps
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
-from typing import List, Optional
-import jwt
-from app import crud
 
 router = APIRouter()
